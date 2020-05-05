f0c904107b452cedbd8d908ee370187463d1f76f
Ben Chacko <bchacko@pivotal.io>
2020-05-05 12:58:02 -0400

    initial commit

--- src/main/java/com/chacko/ben/dnshealthcheck/DnsHealthCheckController.java
+++ src/main/java/com/chacko/ben/dnshealthcheck/DnsHealthCheckController.java
@@ -24,25 +24,39 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class DnsHealthCheckController {
+	String error = "";
 
 	Logger logger = LoggerFactory.getLogger(DnsHealthCheckController.class);
 
 	@RequestMapping(value = "/resolve/{hostname}", method = RequestMethod.GET)
 	String resolveDNS(@PathVariable("hostname") String hostname) {
+		return resolveDNS(hostname, 1);
+	}
+	
+	@RequestMapping(value = "/resolve/{hostname}/{count}", method = RequestMethod.GET)
+	String resolveDNSWithRetries(@PathVariable("hostname") String hostname, @PathVariable("count") int count) {
+		return resolveDNS(hostname, count);
+	}
+
+	String resolveDNS(String hostname, int count) {
 		DnsResolver dnsResolver = new SystemDefaultDnsResolver();
 		StringBuilder builder = new StringBuilder();
 		InetAddress[] addresses;
-		try {
-			addresses = dnsResolver.resolve(hostname);
-			for (int i = 0; i < addresses.length; i++) {
-				String address = addresses[i].toString();
-				logger.info("Rosolved address: " + address);
-				builder.append(address);
-				builder.append("\n");
+		while (count > 0) {
+			try {
+				addresses = dnsResolver.resolve(hostname);
+				for (int i = 0; i < addresses.length; i++) {
+					String address = addresses[i].toString();
+					logger.info("Rosolved address: " + address);
+					builder.append("Resolved address: " + address);
+					builder.append("<br/>");
+				}
+			} catch (UnknownHostException e) {
+				logger.error("Caught UnknownHostException while trying to resolve address: " + hostname);
+				return e.getMessage();
+			} finally {
+				count--;
 			}
-			return builder.toString();
-		} catch (UnknownHostException e) {
-			logger.error("Caught UnknownHostException while trying to resolve address: " + hostname);
 		}
 		return builder.toString();
 	}
